7774c183adf4dd3f25cb8a25531f0d51d9375244
kevvol <jakubkowalewski48@gmail.com>
2023-05-29 22:41:16 +0200

    Dodaj pliki projektów.

---
+++ BrowswerMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Middleware
+{
+    public class BrowswerMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        public BrowswerMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var userAgent = context.Request.Headers["User-Agent"].ToString();
+
+            if(userAgent.Contains("MSIE") || userAgent.Contains("Chromium") || userAgent.Contains("Edg"))
+            {
+                context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/");
+                return;
+            }
+
+            await _requestDelegate(context);
+        }
+    }
+
+    public static class RequestCultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseBrowswerMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BrowswerMiddleware>();
+        }
+    }
+}
